3d3d0b993b586d00072ab55334bc5fa39728da2e
Hakim Njim <njim.hakimnjim1992@gmail.com>
2024-07-31 13:52:44 +0100

    Add bomb effects

--- RPG_Defence/Assets/Scripts/Bullets/Bomb.cs
+++ RPG_Defence/Assets/Scripts/Bullets/Bomb.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Bomb : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject explosionPrefab;
+    [SerializeField] private float explosionRadius = 5f;
+    [SerializeField] private float explosionForce = 700f;
+    [SerializeField] private float damage = 50f;
+
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.transform.tag != "Player" && other.tag != "Bomb")
+        {
+            Explode();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Explode()
     {
-        
+        // Instantiate the explosion effect
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+
+        // Get nearby objects within the explosion radius
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider nearbyObject in colliders)
+        {
+            // Apply explosion force
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null && rb.tag != "Bomb")
+            {
+                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+            }
+
+            // Check for NavMeshAgent and apply damage
+            NavMeshAgent agent = nearbyObject.GetComponent<NavMeshAgent>();
+            if (agent != null)
+            {
+                /*Health health = agent.GetComponent<Health>();
+                if (health != null)
+                {
+                    health.TakeDamage(damage);
+                }*/
+            }
+        }
+
+        // Destroy the bomb
+        Destroy(gameObject);
     }
 }
+
